950be3274921476e17a5bd0260aa35c763f8de67
CihanKurtDev <cihankurt.job@gmail.com>
2026-01-07 12:35:54 +0100

    extended types, before implementing more features

--- src/types/environment.ts
+++ src/types/environment.ts
@@ -6,16 +6,48 @@ export type KPaUnit = 'kPa';
 export type EnvironmentType = 'ROOM' | 'TENT' | 'GREENHOUSE';
 export type TempUnit = '°C' | '°F';
 
+interface ClimateData {
+  temp?: MeasuredValue<TempUnit>;
+  humidity?: MeasuredValue<PercentUnit>;
+  vpd?: MeasuredValue<KPaUnit>;
+  co2?: MeasuredValue<PPMUnit>;
+}
+
 export interface EnvironmentData {
   id: string;
   name: string;
   type: EnvironmentType;
   location?: string;
+  climate?: ClimateData;
+}
 
-  climate?: {
-    temp?: MeasuredValue<TempUnit>;
-    humidity?: MeasuredValue<PercentUnit>;
-    vpd?: MeasuredValue<KPaUnit>;
-    co2?: MeasuredValue<PPMUnit>;
-  };
+export interface EnvironmentData_Historical {
+  id: string;
+  environmentId: string;
+  timetamp: number;
+  climate: ClimateData
+}
+
+export type EnvironmentEventType = 
+  "Climate_Adjustment" 
+  | "Equipment_Change" 
+  | "Cleaning" 
+  | "Maintenance"
+
+export interface EnvironmentEvent {
+  id: string;
+  environmentId: string;
+  timestamp: number;
+  type: EnvironmentEventType;
+  notes?: string;
+
+  climateAdjustment?: {
+    setting: string;
+    target: MeasuredValue<TempUnit | PercentUnit | PPMUnit>
+  }
+
+  equipmentChange?: {
+    equipment: string;
+    action: 'ADDED' | 'REMOVED' | 'REPLACED';
+  }
 }
