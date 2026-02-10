be981d0345af3b1a668756fe717fa7bb75efa8df
CihanKurtDev <cihankurt.job@gmail.com>
2026-02-10 16:19:16 +0100

    fixed double card

--- src/app/environments/[environmentId]/components/shared/DataTab.tsx
+++ src/app/environments/[environmentId]/components/shared/DataTab.tsx
@@ -35,34 +35,32 @@ export default function DataTab({ data, metrics } : { data: any[], metrics: Metr
         {data.length === 0 ? (
           <EmptyState message="Keine Klimadaten vorhanden"/>
           ) : (
-            <Card collapsible={true} title="Klima">
-              <ResponsiveContainer width="100%" height={300}>
-                <LineChart data={data} margin={{ top: 20, right: 30, left: 50, bottom: 20 }}>
-                  <XAxis
-                    dataKey="timestamp"
-                    type="number"
-                    domain={['dataMin', 'dataMax']}
-                    tickFormatter={timestamp => formatDateShort(new Date(timestamp))}
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart data={data} margin={{ top: 20, right: 30, left: 50, bottom: 20 }}>
+                <XAxis
+                  dataKey="timestamp"
+                  type="number"
+                  domain={['dataMin', 'dataMax']}
+                  tickFormatter={timestamp => formatDateShort(new Date(timestamp))}
+                />
+                <YAxis
+                  type="number"
+                />
+                {metrics.map(metric => (
+                  <Line
+                    key={metric.key}
+                    type="monotone"
+                    dataKey={metric.key}
+                    stroke={metric.color}
+                    strokeWidth={1}
+                    dot={{ r: 3}}
+                    name={metric.name}
                   />
-                  <YAxis
-                    type="number"
-                  />
-                  {metrics.map(metric => (
-                    <Line
-                      key={metric.key}
-                      type="monotone"
-                      dataKey={metric.key}
-                      stroke={metric.color}
-                      strokeWidth={1}
-                      dot={{ r: 3}}
-                      name={metric.name}
-                    />
-                  ))}
-                  <Tooltip content={<CustomTooltip />} />
-                  <Legend verticalAlign="top" height={36} iconType="circle" />
-                </LineChart>
-              </ResponsiveContainer>
-            </Card>
+                ))}
+                <Tooltip content={<CustomTooltip />} />
+                <Legend verticalAlign="top" height={36} iconType="circle" />
+              </LineChart>
+            </ResponsiveContainer>
           )
         }
       </Card>
